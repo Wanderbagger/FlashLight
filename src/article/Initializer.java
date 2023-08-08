c73d2235162ec22c3fa33bd396ccfdce8d7bf8ba
Wanderbagger <ronikut1988@mail.ru>
2023-08-08 14:02:45 +0300

    08/08/2023 - продолжение работы над инициализацией УК

--- src/article/Initializer.java
+++ src/article/Initializer.java
@@ -1,15 +1,12 @@
 package article;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
+
 
 public class Initializer {
     public final ArrayList<Article> criminalCode = initializeCriminalCode();
@@ -17,72 +14,47 @@ public class Initializer {
     private Article currentArticle = new Article();
 
     public ArrayList<Article> initializeCriminalCode() {
-
-        BufferedReader reader;
         try {
-            File f = new File("src/CriminalCode.txt");
-            reader = new BufferedReader(new FileReader(f));
-            String line = reader.readLine();
-            while (true) {
-                line = reader.readLine();
-                if(line == null){
-                    break;
-                }
+            List<String> lines = Files.readAllLines(Paths.get("src/CriminalCode.txt"));
+            for (String line: lines) {
                 recognize(line);
             }
-            reader.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
         return tempCriminalCode;
     }
 
-
     private Article recognize(String line) { // распознавание
-        String word = "";
-        System.out.println(line);
-        int spaceCounter = 0;
-        if (line.charAt(0) == 'С' && line.charAt(1) == 'т' && line.charAt(2) == 'а'){
-            for (int i = 5; i < line.length(); i++) {
-                    char c = line.charAt(i);
-                    if (c != ' ') {
-                        word += c;
-                    } else {
-                        System.out.println("Проверка" + word);
-                    }
-            }
-
-
-        } else if(Character.isDigit(line.charAt(0)) && line.charAt(1) == '.'){
+        if (line.charAt(0) == 'С' && line.charAt(1) == 'т'){
+            System.out.println("статья");
+            recognizeArticle(line);
+          } else if(Character.isDigit(line.charAt(0)) && line.charAt(1) == '.'){
             System.out.println("часть");
         } else if(Character.isLetter(line.charAt(0)) && line.charAt(1) == ')'){
             System.out.println("пункт");
         }
-        /*
-        for (int i = 0; i < line.length(); i++) { //перебираем строчку по символу
-            char c = line.charAt(i);
-            if (c != ' ') {
-                word += c;
-            } else {
-                if (word.equals("Статья")) {
-                    setArticleDescription(i, line);
-                } else {
-                    break;
-                }
-            }
-        }
-        */
-
         return currentArticle;
     }
 
-    private void setArticleDescription(int i, String line) {
+    private Article recognizeArticle(String line){
         Article article = new Article();
-        String word = "";
         int spacecounter = 0;
-
-
-
+        String word = "";
+        for (int i = 7; i < line.length(); i++) {
+            if(line.charAt(i) == ' ' && spacecounter == 0){
+                article.setNumber(word);
+                word = "";
+                spacecounter++;
+            } else if (line.charAt(i) == ' ' && spacecounter > 0){
+                word += line.charAt(i);
+                article.setDescription(word);
+            } else {
+                word += line.charAt(i);
+            }
+        }
+        System.out.println(article);
+        return article;
     }
 }
 
