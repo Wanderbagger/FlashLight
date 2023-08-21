6713ecac697f68c9c748eb4a9c4f9eb1376be048
Wanderbagger <90508652+Wanderbagger@users.noreply.github.com>
2023-08-21 22:01:41 +0300

    21/08/2023 - продолжение работы с инициализацией УК

--- src/article/Initializer.java
+++ src/article/Initializer.java
@@ -9,21 +9,17 @@ import java.util.List;
 
 
 public class Initializer {
-    public final ArrayList<Article> criminalCode = new ArrayList<>();;
-    private Article currentArticle;
+    public final ArrayList<Article> criminalCode = new ArrayList<>();
+    private Article currentArticle = new Article();
     private Part currentPart = new Part();
     private Paragraph currentParagraph = new Paragraph();
-    private String nextLineType;
+    private String previousLineType;
 
     public void initializeCriminalCode() {
         try {
             List<String> lines = Files.readAllLines(Paths.get("src/CriminalCode.txt"));
             for (int i = 1; i < lines.size(); i++) {
-                if(lines.get(i).length() > 1) {
-                        if(!chooseType(lines.get(i + 1)).equals("ошибка") && lines.size() > (i + 1)) {
-                            nextLineType = chooseType(lines.get(i + 1));
-                            System.out.println(nextLineType);
-                        }
+                if(lines.get(i).length() > 1 && lines.size() > (i + 1)) {
                     recognize(lines.get(i));
                 }
             }
@@ -31,21 +27,53 @@ public class Initializer {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        for (Article article : criminalCode) {
+            System.out.println(article);
+        }
 
     }
 
-    private Article recognize(String line) { // распознавание
+    private void recognize(String line) { // распознавание
         if (chooseType(line).equals("статья")){
+            if(previousLineType!=null) {
+                if (previousLineType.equals("часть") && currentPart != null) {
+                    currentArticle.parts.add(currentPart);
+                    currentPart = null;
+                } else if (previousLineType.equals("параграф") && currentPart != null && currentParagraph != null) {
+                    currentPart.paragraphs.add(currentParagraph);
+                    currentArticle.parts.add(currentPart);
+                    currentParagraph = null;
+                    currentPart = null;
+                }
+                if(currentArticle!=null){
+                    criminalCode.add(currentArticle);
+                }
+
+                currentArticle = null;
+            }
             currentArticle = recognizeArticle(line);
-            System.out.println(currentArticle);
+            previousLineType = "статья";
+
         } else if(chooseType(line).equals("часть")){
-                currentPart = recognizePart(line);
-                System.out.println(currentPart);
+            if(previousLineType.equals("параграф")){
+                currentPart.paragraphs.add(currentParagraph);
+                currentParagraph = null;
+            }
+
+            if(currentPart != null){
+                currentArticle.parts.add(currentPart);
+            }
+            currentPart = recognizePart(line);
+            previousLineType = "часть";
         } else if(chooseType(line).equals("параграф")){
+
+            if (currentParagraph!=null){
+                currentPart.paragraphs.add(currentParagraph);
+
+            }
             currentParagraph = recognizeParagraph(line);
-            System.out.println(currentParagraph);
+            previousLineType = "параграф";
         }
-        return currentArticle;
     }
 
     private String chooseType (String line) { // распознавание следующей строки
