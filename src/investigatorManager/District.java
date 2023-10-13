c93bcd35226aeb5b473d9aa377e50f3e910a1960
Wanderbagger <ronikut1988@mail.ru>
2023-10-13 13:58:36 +0300

    13/10/2023 - WORK - продолжение работы над InvestigatorManager

---
+++ src/investigatorManager/District.java
@@ -0,0 +1,13 @@
+package investigatorManager;
+
+public enum District {
+    CENTRAL,
+    SOUTHERN,
+    SOUTH_WESTERN,
+    WESTERN,
+    NORTH_WESTERN,
+    NORTHERN,
+    NORTH_EASTERN,
+    EASTERN,
+    SOUTH_EASTERN
+}
